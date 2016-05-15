1e3890699bc32c5522ef8e2880b21c280abbd846
Bernardo Rocha Salgueiro <bernardo.roll@gmail.com>
2016-05-14 21:11:34 -0300

    First commit for Android Benchmark Suite

---
+++ app/src/main/java/tccbenchmarksuite/com/bernardo/tccbenchmarksuite/MainActivity.java
@@ -0,0 +1,13 @@
+package tccbenchmarksuite.com.bernardo.tccbenchmarksuite;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
