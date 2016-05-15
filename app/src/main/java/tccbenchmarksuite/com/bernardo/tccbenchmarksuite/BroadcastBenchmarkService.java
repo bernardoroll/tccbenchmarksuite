68fcc12e90241b77392b6af9d635f9ae2892ae74
Bernardo Rocha Salgueiro <bernardo.roll@gmail.com>
2016-05-15 13:44:03 -0300

    Memória e CPU funcionando. Verificar resultados.

---
+++ app/src/main/java/tccbenchmarksuite/com/bernardo/tccbenchmarksuite/BroadcastBenchmarkService.java
@@ -0,0 +1,168 @@
+package tccbenchmarksuite.com.bernardo.tccbenchmarksuite;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.Service;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.Date;
+
+public class BroadcastBenchmarkService extends Service {
+
+    private final String TAG = "BroadcastBenchmarkSvc";
+
+    public static final String BROADCAST_ACTION = "com.bernardo.tccbenchmarksuite.calculatesage";
+
+    private final Handler handler = new Handler();
+    Intent intent;
+
+    int counter = 0;
+
+    int pid;
+    boolean memoryCheck, cpuCheck, diskCheck;
+
+    public BroadcastBenchmarkService() {
+
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        intent = new Intent(BROADCAST_ACTION);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onStart(Intent i, int startId) {
+        this.pid = i.getIntExtra("pid", -1);
+        this.memoryCheck = i.getBooleanExtra("isMemoryChecked", false);
+        this.cpuCheck = i.getBooleanExtra("isCpuChecked", false);
+        this.diskCheck = i.getBooleanExtra("isDiskChecked", false);
+        handler.removeCallbacks(sendUpdatesToUI);
+        handler.postDelayed(sendUpdatesToUI, 5000);
+    }
+
+    private Runnable sendUpdatesToUI = new Runnable() {
+        public void run() {
+            DisplayLoggingInfo();
+            handler.postDelayed(this, 5000);
+        }
+    };
+
+    private void DisplayLoggingInfo() {
+        Log.d(TAG, ".DisplayLoggingInfo() called.");
+
+        if(memoryCheck) {
+            intent.putExtra("memoryUsage", getMemoryInfo());
+        }
+        if(cpuCheck) {
+            intent.putExtra("cpuUsage", getCpuInfo());
+        }
+        if(diskCheck) {
+            intent.putExtra("diskUsage", getDiskInfo());
+        }
+        intent.putExtra("time", new Date().toLocaleString());
+        intent.putExtra("counter", ++counter);
+        sendBroadcast(intent);
+    }
+
+    private int getMemoryInfo() {
+        ActivityManager aManager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+        for(ActivityManager.RunningAppProcessInfo rAPI : aManager.getRunningAppProcesses()) {
+            if(rAPI.pid == this.pid){
+                android.os.Debug.MemoryInfo[] mi = aManager.getProcessMemoryInfo(
+                        new int[] {this.pid});
+                for (android.os.Debug.MemoryInfo info : mi) {
+                    return info.getTotalPss();
+                }
+            }
+        }
+        return -1;
+    }
+
+    private float getDiskInfo() {
+        return -1;
+    }
+
+    private float getCpuInfo() {
+        try {
+            //Tentar pegar a frequencia do processador
+            ///sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq
+            //RandomAccessFile freqReader = new RandomAccessFile("/sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq", "r");
+            //String freqLoad = freqReader.readLine();
+            //int freqLoad = Integer.parseInt(freqReader.readLine());
+            int USER_HZ = 100;
+
+            RandomAccessFile totalSystemTime = new RandomAccessFile("proc/uptime", "r");
+            float upTime = Float.parseFloat(totalSystemTime.readLine().split(" ")[0]);
+
+            RandomAccessFile reader = new RandomAccessFile("/proc/" + this.pid + "/stat", "r");
+            String load = reader.readLine();
+
+            String[] toks = load.split(" +");  // Split on one or more spaces
+
+//            #14 utime - CPU time spent in user code, measured in clock ticks
+//            #15 stime - CPU time spent in kernel code, measured in clock ticks
+//            #16 cutime - Waited-for children's CPU time spent in user code (in clock ticks)
+//            #17 cstime - Waited-for children's CPU time spent in kernel code (in clock ticks)
+//            #22 starttime - Time when the process started, measured in clock ticks
+
+            long uTime = Long.parseLong(toks[13]);
+            long sTime = Long.parseLong(toks[14]);
+            long cuTime = Long.parseLong(toks[15]);
+            long csTime = Long.parseLong(toks[16]);
+            long startTime = Long.parseLong(toks[21]);
+
+            long total_time = uTime + sTime;
+            total_time = total_time + cuTime + csTime;
+            float seconds = upTime - (startTime / USER_HZ);
+            //float seconds2 = upTime - (startTime / freqLoad);
+
+            float cpu_usage = 100 * ((total_time / USER_HZ) / seconds);
+            //float cpu_usage2 = 100 * ((total_time / freqLoad) / seconds2);
+
+
+            return cpu_usage;
+/*
+
+            long idle1 = Long.parseLong(toks[4]);
+            long cpu1 = Long.parseLong(toks[2]) + Long.parseLong(toks[3]) + Long.parseLong(toks[5])
+                    + Long.parseLong(toks[6]) + Long.parseLong(toks[7]) + Long.parseLong(toks[8]);
+
+            try {
+                Thread.sleep(360);
+            } catch (Exception e) {}
+
+            reader.seek(0);
+            load = reader.readLine();
+            reader.close();
+
+            toks = load.split(" +");
+
+            long idle2 = Long.parseLong(toks[4]);
+            long cpu2 = Long.parseLong(toks[2]) + Long.parseLong(toks[3]) + Long.parseLong(toks[5])
+                    + Long.parseLong(toks[6]) + Long.parseLong(toks[7]) + Long.parseLong(toks[8]);
+
+            return (float)(cpu2 - cpu1) / ((cpu2 + idle2) - (cpu1 + idle1));
+*/
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        return 0;
+    }
+
+
+
+}
--- app/src/main/java/tccbenchmarksuite/com/bernardo/tccbenchmarksuite/MainActivity.java
+++ app/src/main/java/tccbenchmarksuite/com/bernardo/tccbenchmarksuite/MainActivity.java
@@ -1,13 +1,234 @@
 package tccbenchmarksuite.com.bernardo.tccbenchmarksuite;
 
+import android.app.ActivityManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = "MainActivity";
+
+    private CheckBox mCbMemory, mCbCpu, mCbDisk;
+    private EditText mEtPid;
+    private LinearLayout mLlResults;
+
+    private final String ANDROID_STUDIO_PACKAGENAME = "com.bernardo.tccapp";
+    private final String XAMARIN_PACKAGENAME = "com.bernardo.tccappxamarin";
+    private final String APPERY_PACKAGENAME = "com.bernardo.tccappappery";
+
+    Intent broadcastIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initialLoad();
     }
+
+    private void initialLoad() {
+        mCbCpu = (CheckBox) findViewById(R.id.activity_main_cb_cpu);
+        mCbDisk = (CheckBox) findViewById(R.id.activity_main_cb_disk);
+        mCbMemory = (CheckBox) findViewById(R.id.activity_main_cb_memory);
+        mEtPid = (EditText) findViewById(R.id.activity_main_et_pid);
+        mLlResults = (LinearLayout) findViewById(R.id.activity_main_ll_results);
+    }
+
+    public void btnGetAndroidStudioPidClicked(View v) {
+        try {
+//            ActivityManager aManager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+//            int id = -1;
+//            for (ActivityManager.RunningAppProcessInfo rAPI : aManager.getRunningAppProcesses()) {
+//                if (rAPI.processName.equals(ANDROID_STUDIO_PACKAGENAME)) {
+//                    id = rAPI.pid;
+//                    //mEtPid.setText(String.valueOf(rAPI.pid));
+//                }
+//            }
+//            if(id >= 0) {
+//                mEtPid.setText(String.valueOf(id));
+//            } else {
+//                mEtPid.setText("");
+//                Toast.makeText(this, "A aplicação do Android Studio não está em execução.",
+//                        Toast.LENGTH_LONG).show();
+//            }
+            int pId = getPidByPackagename(ANDROID_STUDIO_PACKAGENAME);
+            if(pId >= 0) {
+                mEtPid.setText(String.valueOf(pId));
+            } else {
+                mEtPid.setText("");
+                Toast.makeText(this, "A aplicação do Android Studio não está em execução.",
+                        Toast.LENGTH_LONG).show();
+            }
+        } catch(Exception ex) {
+            Toast.makeText(this, "Não foi possível coletar o PID da aplicação do Android Studio.",
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void btnGetXamarinPidClicked(View v) {
+        try {
+            int pId = getPidByPackagename(XAMARIN_PACKAGENAME);
+            if(pId >= 0) {
+                mEtPid.setText(String.valueOf(pId));
+            } else {
+                mEtPid.setText("");
+                Toast.makeText(this, "A aplicação do Xamarin não está em execução.",
+                        Toast.LENGTH_LONG).show();
+            }
+        } catch(Exception ex) {
+            Toast.makeText(this, "Não foi possível coletar o PID da aplicação do Xamarin",
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void btnGetApperyPidClicked(View v) {
+        try {
+            int pId = getPidByPackagename(APPERY_PACKAGENAME);
+            if(pId >= 0) {
+                mEtPid.setText(String.valueOf(pId));
+            } else {
+                mEtPid.setText("");
+                Toast.makeText(this, "A aplicação do Appery não está em execução.",
+                        Toast.LENGTH_LONG).show();
+            }
+        } catch(Exception ex) {
+            Toast.makeText(this, "Não foi possível coletar o PID da aplicação do Appery",
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private int getPidByPackagename(String packagename) {
+        ActivityManager aManager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+        for(ActivityManager.RunningAppProcessInfo rAPI : aManager.getRunningAppProcesses()) {
+            if(rAPI.processName.equals(packagename)) {
+                return rAPI.pid;
+            }
+        }
+        return -1;
+    }
+
+    private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            showResults(intent);
+        }
+    };
+
+    private void showResults(Intent iResult) {
+
+        TextView tvResult;
+
+        tvResult = new TextView(this);
+        tvResult.setText("------------------------------------");
+        mLlResults.addView(tvResult);
+
+        tvResult = new TextView(this);
+        tvResult.setText("Iteração: " + iResult.getIntExtra("counter", -1));
+        mLlResults.addView(tvResult);
+
+        tvResult = new TextView(this);
+        tvResult.setText("Hora: " + iResult.getStringExtra("time"));
+        mLlResults.addView(tvResult);
+
+        if(mCbMemory.isChecked()) {
+            tvResult = new TextView(this);
+            tvResult.setText("Memória: " + iResult.getIntExtra("memoryUsage", -1));
+            mLlResults.addView(tvResult);
+        }
+
+        if(mCbCpu.isChecked()) {
+            tvResult = new TextView(this);
+            float cpuUsage = iResult.getFloatExtra("cpuUsage", -1);
+            if(cpuUsage >= 0) {
+                tvResult.setText("CPU: " + String.format("%.2f", cpuUsage) + "%");
+            } else {
+                tvResult.setText("CPU: ---");
+            }
+
+            mLlResults.addView(tvResult);
+        }
+
+        if(mCbDisk.isChecked()) {
+            tvResult = new TextView(this);
+            tvResult.setText("Disco: " + iResult.getFloatExtra("diskUsage", -1));
+            mLlResults.addView(tvResult);
+        }
+
+        tvResult = new TextView(this);
+        tvResult.setText("------------------------------------");
+        mLlResults.addView(tvResult);
+
+
+
+    }
+
+    public void btnStopMonitorClicked(View v) {
+        unregisterReceiver(broadcastReceiver);
+        stopService(broadcastIntent);
+
+        mLlResults.removeAllViews();
+
+    }
+
+    public void btnMonitorClicked(View v) {
+//        this.pid = i.getIntExtra("pid", -1);
+//        this.memoryCheck = i.getBooleanExtra("isMemoryChecked", false);
+//        this.cpuCheck = i.getBooleanExtra("isCpuChecked", false);
+//        this.diskCheck = i.getBooleanExtra("isDiskChecked", false);
+        broadcastIntent = new Intent(this, BroadcastBenchmarkService.class);
+        broadcastIntent.putExtra("pid", Integer.parseInt(mEtPid.getText().toString()));
+        broadcastIntent.putExtra("isMemoryChecked", mCbMemory.isChecked());
+        broadcastIntent.putExtra("isCpuChecked", mCbCpu.isChecked());
+        broadcastIntent.putExtra("isDiskChecked", mCbDisk.isChecked());
+
+        startService(broadcastIntent);
+        registerReceiver(broadcastReceiver, new IntentFilter(
+                BroadcastBenchmarkService.BROADCAST_ACTION));
+
+
+
+
+//        ActivityManager aManager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+//
+//        int pid = Integer.parseInt(mEtPid.getText().toString());
+//
+//        boolean isMemoryChecked = mCbMemory.isChecked();
+//        boolean isCpuChecked = mCbCpu.isChecked();
+//        boolean isDiskChecked = mCbDisk.isChecked();
+//
+//        for(ActivityManager.RunningAppProcessInfo rAPI : aManager.getRunningAppProcesses()) {
+//            if(rAPI.pid == pid) {
+//                if(isMemoryChecked) {
+//                    android.os.Debug.MemoryInfo[] mi = aManager.getProcessMemoryInfo(
+//                            new int[] {pid});
+//                    for (android.os.Debug.MemoryInfo info : mi) {
+//
+//                        int totalKB = info.getTotalPss();
+//
+//                        Toast.makeText(this, "Total: " + totalKB, Toast.LENGTH_LONG).show();
+//
+//
+//                    }
+//                }
+//
+//                if(isCpuChecked) {}
+//
+//                if(isDiskChecked) {}
+//            }
+//        }
+
+    }
+
+
+
 }
